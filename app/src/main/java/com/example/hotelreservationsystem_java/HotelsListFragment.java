979ea9d666570d90a6b0be14568d9b8b271d692f
A00474210_MSDA5510 <zunyang.wang@smu.ca>
2024-03-31 23:01:03 -0300

    second commit

---
+++ app/src/main/java/com/example/hotelreservationsystem_java/HotelsListFragment.java
@@ -0,0 +1,117 @@
+package com.example.hotelreservationsystem_java;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class HotelsListFragment extends Fragment implements ItemClickListener {
+
+    View view;
+    TextView headingTextView;
+    ProgressBar progressBar;
+    List<HotelListData> userListResponseData;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.hotel_list_fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //heading text view
+        headingTextView = view.findViewById(R.id.heading_text_view);
+        progressBar = view.findViewById(R.id.progress_bar);
+
+        String checkInDate = getArguments().getString("check in date");
+        String checkOutDate = getArguments().getString("check out date");
+        String numberOfGuests = getArguments().getString("number of guests");
+
+        //Set up the header
+        headingTextView.setText("Welcome user, displaying hotel for " + numberOfGuests + " guests staying from " + checkInDate +
+                " to " + checkOutDate);
+
+
+        ArrayList<HotelListData> hotelListData = initHotelListData();
+        RecyclerView recyclerView = view.findViewById(R.id.hotel_list_recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        HotelListAdapter hotelListAdapter = new HotelListAdapter(getActivity(), hotelListData);
+        recyclerView.setAdapter(hotelListAdapter);
+        //getHotelsListsData();
+    }
+
+    public ArrayList<HotelListData> initHotelListData() {
+        ArrayList<HotelListData> list = new ArrayList<>();
+
+        list.add(new HotelListData("Halifax Regional Hotel", "2000$", "true"));
+        list.add(new HotelListData("Hotel Pearl", "500$", "false"));
+        list.add(new HotelListData("Hotel Amano", "800$", "true"));
+        list.add(new HotelListData("San Jones", "250$", "false"));
+        list.add(new HotelListData("Halifax Regional Hotel", "2000$", "true"));
+        list.add(new HotelListData("Hotel Pearl", "500$", "false"));
+        list.add(new HotelListData("Hotel Amano", "800$", "true"));
+        list.add(new HotelListData("San Jones", "250$", "false"));
+
+        return list;
+    }
+
+
+    private void setupRecyclerView() {
+        progressBar.setVisibility(View.GONE);
+        RecyclerView recyclerView = view.findViewById(R.id.hotel_list_recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        HotelListAdapter hotelListAdapter = new HotelListAdapter(getActivity(), userListResponseData);
+        recyclerView.setAdapter(hotelListAdapter);
+
+        //Bind the click listener
+        hotelListAdapter.setClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view, int position) {
+        HotelListData hotelListData = userListResponseData.get(position);
+
+        String hotelName = hotelListData.getHotel_name();
+        String price = hotelListData.getPrice();
+        String availability = hotelListData.getAvailability();
+
+        Bundle bundle = new Bundle();
+        bundle.putString("hotel name", hotelName);
+        bundle.putString("hotel price", price);
+        bundle.putString("hotel availability", availability);
+
+        HotelGuestDetailsFragment hotelGuestDetailsFragment = new HotelGuestDetailsFragment();
+        hotelGuestDetailsFragment.setArguments(bundle);
+
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.remove(HotelsListFragment.this);
+        fragmentTransaction.replace(R.id.main_layout, hotelGuestDetailsFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commitAllowingStateLoss();
+
+    }
+}
